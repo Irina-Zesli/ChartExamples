7c67b0bf1508709d882240774745b8b2d389f02c
Irina-Zesli <zesli@mail.ru>
2022-11-15 06:17:38 +0300

    Добавление метки заголовка

--- ChartExamples.py
+++ ChartExamples.py
@@ -10,6 +10,10 @@ window = tk.Tk()
 window.geometry('450x450')
 window.title("Примеры построения графиков")
 
+#Добавление метки заголовка
+lblTitle = tk.Label(text = "Примеры построения графиков", font = ('Hevletica', 16, 'bold'), fg = '#0000cc')
+lblTitle.place(x = 55, y = 25)
+
 #Добавление кнопки закрытия программы
 button_close = tk.Button(window, text = "Закрыть", font = ('Hevletica', 10, 'bold'), command = do_close)
 button_close.place(x = 330, y = 400, width = 90, height = 30)
