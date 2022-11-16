8960132f5153007d222a067922c28562ce72629f
Irina-Zesli <zesli@mail.ru>
2022-11-16 22:18:22 +0300

    Добавление нового графика нормальных распределений

--- ChartExamples.py
+++ ChartExamples.py
@@ -39,7 +39,7 @@ lblChart3 = tk.Label(text = "Столбчатая диаграмма")
 lblChart3.place(x = 172, y = 222)
 
 #Добавление кнопки и метки для графика 4
-btnChart2 = tk.Button(window, text = "График 4", font = ('Hevletica', 10, 'bold'), command = chart2.plot_chart)
+btnChart2 = tk.Button(window, text = "График 4", font = ('Hevletica', 10, 'bold'), command = chart2.plot_chart2)
 btnChart2.place(x = 40, y = 265, width = 90, height = 30)
 
 lblChart2 = tk.Label(text = "Нормальное распределение - 3 графика ")
