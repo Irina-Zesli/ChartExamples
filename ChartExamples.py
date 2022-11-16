50fda4f794828b0c63392a6da14b25a94b2b4941
Irina-Zesli <zesli@mail.ru>
2022-11-16 21:48:32 +0300

    Добавление групп кнопка - метка

--- ChartExamples.py
+++ ChartExamples.py
@@ -10,7 +10,7 @@ def do_close():
     window.destroy()
 
 window = tk.Tk()
-window.geometry('450x450')
+window.geometry('450x550')
 window.title("Примеры построения графиков")
 
 #Добавление метки заголовка
@@ -32,15 +32,50 @@ lblChart2 = tk.Label(text = "Нормальное распределение")
 lblChart2.place(x = 170, y = 172)
 
 #Добавление кнопки и метки для графика 3
-btnChart3 = tk.Button(window, text = "График 3", font = ('Hevletica', 10, 'bold'), command = chart3.plot_chart)
+btnChart3 = tk.Button(window, text = "График 3", font = ('Hevletica', 10, 'bold'), command = chart3                                   .plot_chart)
 btnChart3.place(x = 40, y = 215, width = 90, height = 30)
 
 lblChart3 = tk.Label(text = "Столбчатая диаграмма")
 lblChart3.place(x = 172, y = 222)
 
+#Добавление кнопки и метки для графика 4
+btnChart2 = tk.Button(window, text = "График 4", font = ('Hevletica', 10, 'bold'), command = chart2.plot_chart)
+btnChart2.place(x = 40, y = 265, width = 90, height = 30)
+
+lblChart2 = tk.Label(text = "Нормальное распределение - 3 графика ")
+lblChart2.place(x = 170, y = 272)
+
+#Добавление кнопки и метки для графика 5
+btnChart2 = tk.Button(window, text = "График 5", font = ('Hevletica', 10, 'bold'), command = chart2.plot_chart)
+btnChart2.place(x = 40, y = 315, width = 90, height = 30)
+
+lblChart2 = tk.Label(text = "Описание графика ")
+lblChart2.place(x = 170, y = 322)
+
+#Добавление кнопки и метки для графика 6
+btnChart2 = tk.Button(window, text = "График 6", font = ('Hevletica', 10, 'bold'), command = chart2.plot_chart)
+btnChart2.place(x = 40, y = 365, width = 90, height = 30)
+
+lblChart2 = tk.Label(text = "Описание графика ")
+lblChart2.place(x = 170, y = 372)
+
+#Добавление кнопки и метки для графика 7
+btnChart2 = tk.Button(window, text = "График 7", font = ('Hevletica', 10, 'bold'), command = chart2.plot_chart)
+btnChart2.place(x = 40, y = 415, width = 90, height = 30)
+
+lblChart2 = tk.Label(text = "Описание графика ")
+lblChart2.place(x = 170, y = 422)
+
+#Добавление кнопки и метки для графика 8
+btnChart2 = tk.Button(window, text = "График 8", font = ('Hevletica', 10, 'bold'), command = chart2.plot_chart)
+btnChart2.place(x = 40, y = 465, width = 90, height = 30)
+
+lblChart2 = tk.Label(text = "Описание графика ")
+lblChart2.place(x = 170, y = 472)
+
 #Добавление кнопки закрытия программы
 button_close = tk.Button(window, text = "Закрыть", font = ('Hevletica', 10, 'bold'), command = do_close)
-button_close.place(x = 330, y = 400, width = 90, height = 30)
+button_close.place(x = 330, y = 500, width = 90, height = 30)
 
 window.mainloop()
 
